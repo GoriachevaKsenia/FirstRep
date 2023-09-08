35690c9b83b8d9e965c4be9df2febbfef0b8b4bb
GoriachevaKsenia <Ksenia5Goriacheva@yandex.ru>
2023-09-08 16:39:53 +0300

    Sum

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 void main(){
-    printf ("Hello world!\n");
+    int a, b;
+    printf ("Enter 2 num:\n");
+    scanf("%d %d",  &a, &b);
+    printf ("We get sum:\n");
+    printf("%d\n", a+b);
 }
-
-
